6432cfb4694aaf8081d151014771be4815deb714
mcastan0 <m.andres2403@gmail.com>
2020-12-23 01:00:54 -0500

    Adding TeamCard Vuetify

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>    
+        <div>
+            <v-card
+                class="mx-auto"
+                max-width="344"
+                >
+                <v-img
+                   :src="member.image"
+                    height="200px"
+                    width="250px"
+                ></v-img>
+            
+                <v-card-title>
+                   {{member.nombre}}
+                </v-card-title>
+            
+                <v-card-subtitle>
+                    {{member.rol}}
+                </v-card-subtitle>
+            
+                <v-card-actions>
+                    <v-btn
+                    color="orange lighten-2"
+                    text
+                    >
+                    Conoceme
+                    </v-btn>
+            
+                    <v-spacer></v-spacer>
+            
+                    <v-btn
+                    icon
+                    @click="show = !show"
+                    >
+                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                    </v-btn>
+                </v-card-actions>
+            
+                <v-expand-transition>
+                    <div v-show="show">
+                    <v-divider></v-divider>
+            
+                    <v-card-text>
+                        {{member.descripcion}}
+                    </v-card-text>
+                    </div>
+                </v-expand-transition>
+                </v-card>
+
+        </div>
+     
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TeamCard",
+        props: ['member'],
+
+        data: () => ({
+        show: false,
+             }),
+        
+       }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,16 @@
     <informacion></informacion>
     <articulos></articulos>
     <casos-exito></casos-exito>
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <br>
+        <h2 class="text-center">Equipo</h2>
+        <div class="col mt-5" v-for="(miembro,index) of Team" :key="index">
+            <team-card :member="miembro"></team-card>            
+        </div>        
+      </div>
+    </div>
+
     <seccion-footer></seccion-footer>
   </div>
 </template>
@@ -14,6 +24,7 @@ import BannerServicios from '../components/BannerServicios.vue'
 import CasosExito from '../components/CasosExito.vue'
 import Informacion from '../components/Informacion.vue'
 import SeccionFooter from '../components/SeccionFooter.vue'
+import TeamCard from '../components/TeamCard.vue'
 
 export default {
   name: 'Home',
@@ -22,7 +33,53 @@ export default {
     Articulos,
     CasosExito,
     Informacion,
-    SeccionFooter
-  }
+    SeccionFooter,
+    TeamCard
+  },
+   data() {
+    return {
+
+      Team: [
+           {
+        codigo: 1,
+        nombre: "Kevin Andres Balcazar",
+        descripcion:
+          "Vivo en Montería, estudiante de programación (Amateur). Mi comida favorita es el perro suizo. Mi película favorita es la saga predator. Mi nickname es Clark Kent.",
+        rol: "Arquitecto de Software",
+        image: "https://i.imgur.com/h1ibIMZ.jpg",
+        correo: "kevin_balcazar9@hotmail.com",
+      },
+      {
+        codigo: 2,
+        nombre: "Deisy Katherine Pineda",
+        descripcion:
+          "Vivo en Bogotá. Estudiante de ing. mecatrónica. Mi comida favorita es pizza mexicana y lasaña. Mi película favorita es los 13 fantasmas. Mi nickname es Kathe o Kata.",
+        rol: "Desarrollador Frontend",
+        image: "https://i.imgur.com/zAoJz8Y.jpg",
+        correo: "kte_0330@hotmail.com",
+      },
+      {
+        codigo:3,
+        nombre:"Andres Castaño",
+        descripcion:"Vivo en Cali. Tecnologo Electrónico. Mi comida favorita la lasagna. Apasionado por el cine y la investigación. Mi peliculas favorita Interstellar. Mi nickname Mich.",
+        rol:"Desarrollador Backend",
+        image:"https://i.ibb.co/qxdLFkQ/icon3.jpg"
+    },
+      {
+        codigo: 4,
+        nombre: "Jhon Jairo Sanabria",
+        descripcion:
+          "Vivo en Bogotá. Mi profesión ingeniero electrónico. Mi comida favorita es la pasta, wraps y sushi. Mi película favorita es Matrix. Mi nickname es JJ",
+        rol: "Analista Tester",
+        image: "https://i.imgur.com/FPDZUXl.jpg",
+        correo: "auyeco@hotmail.com",
+      }
+      ],
+      
+
+
+
+    };
+  },
 }
 </script>
